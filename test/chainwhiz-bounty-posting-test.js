560819c750d5def69ecebdde9f54f26384d26011
akp111 <pradhanashish95@gmail.com>
2021-10-30 00:22:00 +0530

    solution-posting

--- test/chainwhiz-bounty-posting-test.js
+++ test/chainwhiz-bounty-posting-test.js
@@ -17,24 +17,24 @@ describe("ChainwhizCore Bounty Posting --> postIssue validations", function () {
 
   it("Should revert with error for insufficient balance", async function () {
     const trxObj = chainwhiz.connect(a1).postIssue("abcd","www.google.com", tokensBN(10000),tokensBN(10000), 123456, 23456, 8910, true, { value: tokensBN(20000) });
-    expect(trxObj).to.be.revertedWith("ChainwhizCore Error: User doesnt have enough balance")
+    expect(trxObj).to.be.revertedWith("ChainwhizCore Error in postIssue: User doesnt have enough balance")
   });
 
   it("Should revert with error for setting reward less than threshold", async function () {
     const trxObj = chainwhiz.connect(a2).postIssue("efg","www.google.com", tokensBN(1),tokensBN(2), 123456, 23456, 8910, true, { value: tokensBN(3) });
-    expect(trxObj).to.be.revertedWith("ChainwhizCore Error: Reawrd amount cannot be less than MIN_REWARD_AMOUNT")
+    expect(trxObj).to.be.revertedWith("ChainwhizCore Error in postIssue: Reawrd amount cannot be less than MIN_REWARD_AMOUNT")
   });
 
   it("Should revert with error for not transferring sufficient funds", async function () {
     const trxObj = chainwhiz.connect(a2).postIssue("efg","www.google.com", tokensBN(10),tokensBN(2), 123456, 23456, 8910, true, { value: tokensBN(7) });
-    expect(trxObj).to.be.revertedWith("ChainwhizCore Error: User didnt transfer sufficient funds")
+    expect(trxObj).to.be.revertedWith("ChainwhizCore Error in postIssue: User didnt transfer sufficient funds")
   });
 
   it("Should revert with error for linking same github id with different account for posting issue", async function () {
    expect(chainwhiz.connect(a2).postIssue("efg","www.google.com", tokensBN(10),tokensBN(2), 123456, 23456, 8910, true, { value: tokensBN(12) })).to.be.emit(chainwhiz,"IssuePosted");
 
    const trxObj = chainwhiz.connect(a3).postIssue("efg","www.google.com", tokensBN(10),tokensBN(2), 123456, 23456, 8910, true,{ value: tokensBN(12) });
-   expect(trxObj).to.be.revertedWith("ChainwhizCore Error: The address linked github id is not the same");
+   expect(trxObj).to.be.revertedWith("ChainwhizCore Error in postIssue: The address linked github id is not the same");
   });
 
 });
